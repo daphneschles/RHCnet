6b97e7eda83fb51283a54e634a9213a472000c40
Daphne Schlesinger <dschles@mit.edu>
2021-11-22 15:12:42 -0500

    remove incorrect preprocessing function

--- Utils/network_tools.py
+++ Utils/network_tools.py
@@ -62,20 +62,3 @@ def make_final_net(pre_trained_loc=default_pretrained_model_loc, model_loc=defau
     full_model = load_weights(full_model, model_loc=model_loc)
     
     return full_model
-
-
-'''
-Pre-process an array of ECGs of shape  N_ECGs x N_samples x 1
-N_samples is the number of samples in a single ECG
-'''
-def pre_process_ECG(ecg) :
-    
-    # first, normalize each signal 
-    ecg_norm = (ecg - ecg.mean(axis=0))/ecg.std(axis=0)
-    ecg_norm = np.expand_dims(ecg_norm, 2)
-    
-    # tile the signal so it is the right shape for the model
-    ecg_tile = np.tile(ecg_norm, [1,1,12])
-    
-    return ecg_tile
-    
